c6d2ddfcbbbd96fe85d9c46cd30ebf5e868cd922
Pablo Witt <pablowitt321@gmail.com>
2022-04-16 16:53:17 -0300

    treinamentos newv2

--- src/br/com/fiap/tds/Exercicio.java
+++ src/br/com/fiap/tds/Exercicio.java
@@ -1,9 +1,24 @@
 package br.com.fiap.tds;
 
 public class Exercicio {
+	/** Atributos */
 	String name;
-	
 
+	
+	/** Metodo para pesquisar o exercicio */
+	public String toString() {
+		return "Exercicio [name=" + name + "]";
+	}
+
+	/** Metodos */
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 }
 
--- src/br/com/fiap/tds/Treinamento.java
+++ src/br/com/fiap/tds/Treinamento.java
@@ -1,5 +1,41 @@
 package br.com.fiap.tds;
+import java.util.Date;
+
 
 public class Treinamento {
+	/** Atributos */
+	private Date duracao;
+	private String intensidade;
+	private int totalCalorias;
+	public Date getDuracao() {
+		return duracao;
+	
+	/** Metodo para visualizar o treinamento */
+	public String toString() {
+		return "Treinamento [duracao=" + duracao + ", intensidade=" + intensidade + ", totalCalorias=" + totalCalorias
+					+ "]";
+	
+	
+	/** Metodos Getters/Setters */
+	}
 
-}
+	}
+	public void setDuracao(Date duracao) {
+		this.duracao = duracao;
+	}
+	public String getIntensidade() {
+		return intensidade;
+	}
+	public void setIntensidade(String intensidade) {
+		this.intensidade = intensidade;
+	}
+	public int getTotalCalorias() {
+		return totalCalorias;
+	}
+	public void setTotalCalorias(int totalCalorias) {
+		this.totalCalorias = totalCalorias;
+	}
+	
+	
+	
+	}
