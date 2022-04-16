5b238865796d66a7fb5b53368d7a5a45ce99d8e4
Pablo Witt <pablowitt321@gmail.com>
2022-04-16 16:43:07 -0300

    treinamentos new

--- src/br/com/fiap/tds/ComposicaoTreino.java
+++ src/br/com/fiap/tds/ComposicaoTreino.java
@@ -1,5 +1,25 @@
 package br.com.fiap.tds;
+import java.util.Date;
 
 public class ComposicaoTreino {
+	/** Atributos */
+	Date duracao;
+	Date tempo;
+	
+	/** Metodos */
+	public Date getDuracao() {
+		return duracao;
+	}
+	public void setDuracao(Date duracao) {
+		this.duracao = duracao;
+	}
+	public Date getTempo() {
+		return tempo;
+	}
+	public void setTempo(Date tempo) {
+		this.tempo = tempo;
+	}
+	
+	
 
 }
