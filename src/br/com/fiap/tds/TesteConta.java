6cca8530b14a1ca54b84ba8dd41231ccf6f203c0
EllenCGomes <ellen_gomes14@hotmail.com>
2022-04-18 21:04:48 -0300

    instancia

--- src/br/com/fiap/tds/TesteConta.java
+++ src/br/com/fiap/tds/TesteConta.java
@@ -1,17 +1,18 @@
 package br.com.fiap.tds;
+import java.util.Date;
 
 public class TesteConta {
 	public static void main(String[]args) {
-		Conta c1 = new Conta();
-		
-		c1.username = "EllenCGomes";
-		c1.idioma = "English";
-		c1.cliente.nome = "Ellen";
-		c1.cliente.idade = 31;
-		
-		System.out.println(c1.cliente.nome);
-		System.out.println(c1.idioma);
-		System.out.println(c1.username);
+		Usuario user1 = new Usuario();
+		Date nascimento = new Date();
+		user1.adicionarUsuario("Haka", nascimento, "masculino", "estudante", 1.69f);
+		user1.editarAltura(1.71f);
+		user1.editarGenero("Nao desejo informar");
+		user1.editarIdade(nascimento);
+		user1.editarNome("Grupo Haka");
+		user1.editarProfissao("desenvolvedor");
+		user1.visualizarUsuario();
+		user1.deletarUsuario();
 		
 	}
 }
